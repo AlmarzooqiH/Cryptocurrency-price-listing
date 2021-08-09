7c81a777f86d31228815a918d9abb4b57d6b6bd5
Hamad Almarzooqi <AlmarzooqiH23@gmail.com>
2021-08-09 18:23:03 +0400

    Initial Commit

---
+++ Cryptocurrency price listing/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cryptocurrency price listing
+//
+//  Created by Hamad Saleh Almarzooqi on 09/08/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
